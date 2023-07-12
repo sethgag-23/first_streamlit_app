3a15968a305a5dfc9538765393fa12989b581670
sethgag-23 <139311340+sethgag-23@users.noreply.github.com>
2023-07-12 19:21:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-import snokflake.connector
+import snowkflake.connector
